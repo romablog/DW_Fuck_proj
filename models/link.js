59c7e51193c54cca6604a8a9765e59bc69b13164
romablog <iromablog@gmail.com>
2016-01-25 13:58:04 +0300

    commit brain

---
+++ models/link.js
@@ -0,0 +1,71 @@
+var async = require('async');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+var mongoose = require('../libs/mongoose'),
+    Schema = mongoose.Schema;
+
+var schema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    fileName: {
+        type: String,
+        required: true
+    },
+    fileText: {
+        type: String,
+        required: true
+    }
+});
+
+schema.statics.allUsersFile = function(username, fileName, callback) {
+    var File = this;
+    async.waterfall([
+        function(callback) {
+            File.findOne({username: username, fileName: fileName}, callback);
+        },
+        function(file, callback) {
+            if (file) {
+                callback(file.fileText);
+            } else {
+                callback("No Text");
+            }
+        }
+    ], callback);
+};
+
+schema.statics.findFile = function(username, fileName, callback) {
+    var File = this;
+    async.waterfall([
+        function(callback) {
+            File.findOne({username: username, fileName: fileName}, callback);
+        },
+        function(file, callback) {
+            if (file) {
+                callback(file.fileText);
+            } else {
+                callback("No Text");
+            }
+        }
+    ], callback);
+};
+
+schema.statics.addFile = function(username, fileName, fileText, callback) {
+    var File = this;
+    async.waterfall([
+        function(callback) {
+            File.findOne({username: username, fileName: fileName}, callback);
+        },
+        function(file, callback) {
+            if (file) {
+                File.remove(file, callback);
+            }
+            var newFile = new File({username: username, fileName: fileName, fileText: fileText});
+            newFile.save();
+        }
+    ], callback);
+};
+
+exports.File = mongoose.model('File', schema);
